e07a6e7c1f2ce051a83a1308e27b3bc141539795
dmcongitus <dmcongitus@gmail.com>
2018-10-23 22:47:26 +0700

    fix db

--- db/qldd.sql
+++ db/qldd.sql
@@ -1,15 +1,12 @@
 /*
 Navicat MySQL Data Transfer
-
 Source Server         : localhost
 Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : qldd
-
 Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
-
 Date: 2017-05-08 13:11:13
 */
 
